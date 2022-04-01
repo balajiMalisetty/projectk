25df1a167f250545e1923cd966228b1fd8d3b35e
2000030560_balaji <2000030560@kluniversity.in>
2022-04-01 07:01:18 +0530

    com

--- fhs/pages/views.py
+++ fhs/pages/views.py
@@ -45,87 +45,121 @@ def _cart_id(request):
     return cart
 
 def add_cart(request, food_id):
-    food = Food.objects.get(id=food_id)
     try:
-        cart = Cart.objects.get(cart_id=_cart_id(request))
-    except Cart.DoesNotExist:
-        cart = Cart.objects.create(
-            cart_id = _cart_id(request)
-        )
-        cart.save()
-    try:
-        cart_item = CartItem.objects.get(item=food, cart=cart)
+        food = Food.objects.get(id=food_id)
 
-        cart_item.quantity += 1
-        cart_item.save()
-    except CartItem.DoesNotExist:
-        cart_item = CartItem.objects.create(
-            item=food,
-            quantity=1,
-            cart=cart
-        )
-        cart_item.save()
-    return redirect('cart')
+        try:
+            cart = Cart.objects.get(cart_id=_cart_id(request))
+        except Cart.DoesNotExist:
+            cart = Cart.objects.create(
+                    cart_id = _cart_id(request)
+            ) 
+            cart.save()
+        try:
+        
+            cart_item = CartItem.objects.get(item=food, cart=cart)
+            cart_item.quantity += 1
+            cart_item.save()           
 
-def add_cart_diet(request, food_id):
-    food = Diet.objects.get(id=food_id)
-    try:
-        cart = Cart.objects.get(cart_id=_cart_id(request))
-    except Cart.DoesNotExist:
-        cart = Cart.objects.create(
-            cart_id = _cart_id(request)
-        )
-        cart.save()
-    try:
-        cart_item = CartItemDiet.objects.get(item=food, cart=cart)
+        except CartItem.DoesNotExist:
+            cart_item = CartItem.objects.create(
+                item=food,
+                quantity=1,
+                cart=cart
+            )
+            cart_item.save()
+        return redirect('cart')
 
-        cart_item.quantity += 1
-        cart_item.save()
-    except CartItemDiet.DoesNotExist:
-        cart_item = CartItemDiet.objects.create(
-            item=food,
-            quantity=1,
-            cart=cart
-        )
-        cart_item.save()
-    return redirect('cart')
+    except Food.DoesNotExist:
+        food =  Diet.objects.get(id=food_id)
+        try:
+            cart = Cart.objects.get(cart_id=_cart_id(request))
+        except Cart.DoesNotExist:
+            cart = Cart.objects.create(
+                cart_id = _cart_id(request)
+            ) 
+            cart.save()
+        try:
+            cart_item = CartItemDiet.objects.get(item=food, cart=cart)
+            cart_item.quantity += 1
+            cart_item.save()
+        except CartItemDiet.DoesNotExist:
+            cart_item = CartItemDiet.objects.create(
+                item=food,
+                quantity=1,
+                cart=cart
+            )
+            cart_item.save()
+        return redirect('cart')
+
+
 
 def cart(request, total=0, counter=0, cart_items=None):
-    
-    cart = Cart.objects.get(cart_id=_cart_id(request))
+    try:
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+    except:
+        return render(request,'pages/empcart.html')
     cart_items = CartItem.objects.filter(cart=cart, active=True)
     cart_itemsD = CartItemDiet.objects.filter(cart=cart, active=True)
-    if not cart_items:
+    if cart_items and cart_itemsD:
+        for cart_item in cart_items:
+            total += (cart_item.item.Food_price * cart_item.quantity)
+            counter += cart_item.quantity
         for cart_item in cart_itemsD:
             total += (cart_item.item.Food_price * cart_item.quantity)
             counter += cart_item.quantity
-            context = {'cart_items': cart_itemsD, 'total': total, 'counter': counter}
-            return render(request, 'pages/cart.html', context)
+        context = {'cart_items': cart_items,'cart_itemsD': cart_itemsD, 'total': total, 'counter': counter}
+        return render(request, 'pages/cart.html', context)
+    elif not (cart_items):
+        if not (cart_itemsD):
+            return render(request,'pages/empcart.html')
+        for cart_item in cart_itemsD:
+            total += (cart_item.item.Food_price * cart_item.quantity)
+            counter += cart_item.quantity
+        context = {'cart_items': cart_itemsD, 'total': total, 'counter': counter}
+        return render(request, 'pages/cart.html', context)
+        
     else:
         for cart_item in cart_items:
             total += (cart_item.item.Food_price * cart_item.quantity)
             counter += cart_item.quantity
-            context = {'cart_items': cart_items, 'total': total, 'counter': counter}
-            return render(request, 'pages/cart.html', context)
+        context = {'cart_items': cart_items, 'total': total, 'counter': counter}
+        return render(request, 'pages/cart.html', context)
 
 def cart_remove(request, product_id):
     cart = Cart.objects.get(cart_id=_cart_id(request))
-    product = get_object_or_404(Food, id=product_id)
-    cart_item = CartItem.objects.get(item=product, cart=cart)
-    if cart_item.quantity > 1:
-        cart_item.quantity -= 1
-        cart_item.save()
-    else:
-        cart_item.delete()
-    return redirect('cart')
+    try:
+        product = get_object_or_404(Food, id=product_id)
+        cart_item = CartItem.objects.get(item=product, cart=cart)
+        if cart_item.quantity > 1:
+            cart_item.quantity -= 1
+            cart_item.save()
+        else:
+            cart_item.delete()
+        return redirect('cart')
+    except:
+        product = get_object_or_404(Diet, id=product_id)
+        cart_item = CartItemDiet.objects.get(item=product, cart=cart)
+        if cart_item.quantity > 1:
+            cart_item.quantity -= 1
+            cart_item.save()
+        else:
+            cart_item.delete()
+        return redirect('cart')        
 
 
 def cart_remove_product(request, product_id):
     cart = Cart.objects.get(cart_id=_cart_id(request))
-    product = get_object_or_404(Food, id=product_id)
-    cart_item = CartItem.objects.get(item=product, cart=cart)
-    cart_item.delete()
-    return redirect('cart')
+    try:
+        product = get_object_or_404(Food, id=product_id)
+        cart_item = CartItem.objects.get(item=product, cart=cart)
+        cart_item.delete()
+        return redirect('cart')
+    except:
+        product = get_object_or_404(Diet, id=product_id)
+        cart_item = CartItemDiet.objects.get(item=product, cart=cart)
+        cart_item.delete()
+        return redirect('cart')       
 
 
 def diet(request):
